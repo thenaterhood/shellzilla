d65b2ef4004c474e28ae254786bb6070022dfba1
Nate Levesque <public@thenaterhood.com>
2012-11-14 22:13:32 -0500

    add display_configurator, license, devswitch, update README

---
+++ devswitch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#############################
+# Nate Levesque
+# 11/14/2012
+# Switch to turn an input device on and off.  
+#
+#############################
+
+device_name=touchpad
+# Gets the device id for the device
+deviceid=`xinput | grep -i $device_name | cut -d"=" -f2 | cut -c 1-2`
+
+# Automatically discovers the device id of the touchpad.
+# comment above and uncomment below to set it manually or to use a 
+# different device
+#deviceid=10
+
+# Gets the current status of the device
+status=`xinput list-props $deviceid | grep "Device Enabled" | cut -c 24`
+
+# Checks to make sure script dependencies are satisfied
+if [ "`whereis xinput`" == "xinput:" ]; then
+    echo "You need to install xinput to use this script."
+    exit 1
+fi
+
+# Changes it to the opposite status
+if [ "$status" = 1 ]; then
+    xinput set-int-prop $deviceid "Device Enabled" 8 0
+    export DISPLAY=:0.0 && notify-send -i /usr/share/icons/gnome/scalable/devices/input-touchpad-symbolic.svg $device_name "$device_name disabled"
+elif [ "$status" = 0 ]; then
+    xinput set-int-prop $deviceid "Device Enabled" 8 1
+    export DISPLAY=:0.0 && notify-send -i /usr/share/icons/gnome/scalable/devices/input-touchpad-symbolic.svg $device_name "$device_name enabled"
+fi
+
+exit 0
---
+++ display_configurator.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Nate Levesque
+# Deals with extending or cloning the desktop 
+# between the internal and 1 external monitor 
+# with xrandr
+# 11/12/2012
+#
+
+#
+# Basic options
+monitor_direction=left
+default_monitor=LVDS1
+
+#
+# Checks to make sure script dependencies are satisfied
+if [ "`whereis xrandr`" == "xrandr:" ]; then
+    echo "You need to install xrandr to use this script."
+    exit 1
+fi
+
+if [ "`whereis dialog`" == "dialog:" ]; then
+    echo "You need to install dialog to use this script."
+    exit 1
+fi
+
+# Lists available ports (ones with something connected) in dialog and
+# display options.  Returns them both to radioOptions, space separated
+
+radioOptions=$(dialog --output-fd 1 --backtitle "Available Display Ports" --radiolist "Select port:" 12 40 4 `xrandr | grep -w "connected" | sed 's/ .*//g' | awk '{print NR " " $0 " off"}' | sed ':a;N;$!ba;s/\n/ /g'`--and-widget --backtitle "Display Options" --radiolist "Select an option:" 12 40 4 1 "Extend" on 2 "Clone" off 3 "Switch display" off 4 "Normal display" off)
+
+#
+# Sets relevant variables
+number=`echo $radioOptions | cut -c 1`
+disp_opt=`echo $radioOptions | cut -c 3`
+exterior_port=`xrandr | grep -w "connected" | sed 's/ .*//g' | head -$number | tac | head -1`
+builtin_monitor=$default_monitor
+echo $disp_opt
+echo $number
+#
+# Sets display option using xrandr
+if [ "$disp_opt" = 1 ]; then
+    # Extends the display to the right
+    xrandr --output $exterior_port --off --output $builtin_monitor --auto &&
+    xrandr --output $exterior_port --auto --output $builtin_monitor --auto --$monitor_direction-of $exterior_port
+elif [ "$disp_opt" = 2 ]; then
+    # Clones the display across both monitors
+    xrandr --output $exterior_port --off --output $builtin_monitor --auto &&
+    xrandr --output $exterior_port --auto
+elif [ "$disp_opt" = 3 ]; then
+    # Switches to the selected display
+    xrandr --output $exterior_port --off --output $builtin_monitor --auto &&
+    xrandr --output $builtin_monitor --off --output $exterior_port --auto
+elif [ "$disp_opt" = 4 ]; then
+    # Returns to the normal (builtin) display
+    xrandr --output $exterior_port --off --output $builtin_monitor --auto
+fi
+
+clear
+exit 0
