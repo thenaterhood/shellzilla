7b296456695321921605a95ebe34c5223bf0e25b
Nate Levesque <public@thenaterhood.com>
2012-12-19 23:09:19 -0500

    code cleanup, add ability to report and record errors

--- system_backup.sh
+++ system_backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #
+# Author: Nate Levesque <public@thenaterhood.com>
+# Language: Shell
+# Filename: system_backup.sh
 #
-# Backup Script
-# Nate Levesque
-# Last updated 03/17/2012
+# Description:
+#   Basic script that implements a couple of backups
+#   for user files and the OS.  Might require a couple changes
+#   in the Basic Settings section depending on your setup.
 #
 # This is online mainly to give me incentive to clean it up
 # 
@@ -21,6 +25,32 @@ day=`date +%d`
 month=`date +%b`
 year=`date +%Y`
 
+# Defining a few functions to use later
+createInfo(){
+    # Appends information about the backup to the backup file
+    # date, and a list of excluded files and folders
+    echo `date` >> $backup_path/$backup_type/$backup_name/$info_file
+    echo "Excluded:" >> $backup_path/$backup_type/$backup_name/$info_file
+    cat .exclude >> $backup_path/$backup_type/$backup_name/$info_file
+}
+
+completed(){
+    # Reports backup completed successfully
+    dialog --title "Backup Complete" \
+    --msgbox "Backup complete- backup set $backup_name" 10 30;
+    
+    echo "Backup completed with no problems" > $backup_path/$backup_type/$backup_name/$info_file
+    createInfo
+}
+
+completedWithErrors(){
+    # Reports backup completed with errors
+    dialog --title "Backup Complete (with errors)" \
+    --msgbox "Backup completed with errors- backup set $backup_name" 10 30;
+    
+    echo "Backup completed with errors" > $backup_path/$backup_type/$backup_name/$info_file
+    createInfo
+}
 
 # Implements a few basic dependency checks
 if [ ! `command -v dialog` ]; then
@@ -40,7 +70,10 @@ fi
 
 # Asks for the backup drive semi-graphically with dialog
 dialog --backtitle "Backup" --msgbox "Mount media device and hit enter." 5 40
-NUM=$(dialog --output-fd 1 --backtitle "Backup" --radiolist "Select device:" 12 40 4 0 /dev/null off `ls $mountpt | sed 's/ .*//g' | awk '{print NR " " $0 " off"}' | sed ':a;N;$!ba;s/\n/ /g'`)
+NUM=$(dialog --output-fd 1 --backtitle "Backup" --radiolist \
+    "Select device:" 12 40 4 0 /dev/null off `ls $mountpt | sed 's/ .*//g' \
+    | awk '{print NR " " $0 " off"}' | sed ':a;N;$!ba;s/\n/ /g'`)
+
 drive="`ls $mountpt | head -$NUM | tac | head -1`"
 
 # Pieces together filenames for the backup and info
@@ -52,31 +85,34 @@ info_file="$HOSTNAME-$month-$day-$year.info"
 cd /
 
 # Ask what backup to perform- rsync /home/$USER or tarball OS
-backup_option=$(dialog --output-fd 1 --backtitle "Backup Options" --radiolist "Select one:" 12 40 4 1 "home sync" off 2 "OS image (requires root)" off)
+backup_option=$(dialog --output-fd 1 --backtitle "Backup Options" --radiolist \
+    "Select one:" 12 40 4 1 "home sync" off 2 "OS image (requires root)" off)
+
 backup_path="$mountpt/$drive/backups"
+
+
 
 # Runs the actual backup
 case "$backup_option" in
     1)
         backup_type="users"
         backup_name=$USER
-        info_file="$backup_name-HOME-$USER.info"
-        rsync -avhL --ignore-errors --delete --delete-after --recursive /home/$USER/ $backup_path/$backup_type/$USER/ --exclude-from /home/$USER/.exclude --delete-excluded --progress
-    ;;
+        info_file="$backup_name-backup.info"
+        rsync -avhL --ignore-errors --delete --delete-after \
+            --recursive /home/$USER/ $backup_path/$backup_type/$USER/ \
+            --exclude-from /home/$USER/.exclude --delete-excluded --progress && completed\
+            || completedWithErrors
+        ;;
     2)
         backup_type="os"
         mkdir $mountpt/$drive/backups/$backup_type/$backup_name
-        sudo tar cvpzf $backup_path/$backup_type/$backup_name/$backup_file --exclude=/proc --exclude=/lost+found --exclude=/media --exclude=/run --exclude=/mnt --exclude=/sys --exclude=/dev/shm --exclude=/tmp --exclude=/home /
-    ;;
+        sudo tar cvpzf $backup_path/$backup_type/$backup_name/$backup_file \
+            --exclude=/proc --exclude=/lost+found --exclude=/media \
+            --exclude=/run --exclude=/mnt --exclude=/sys \
+            --exclude=/dev/shm --exclude=/tmp --exclude=/home / && completed\
+            || completedWithErrors
+        ;;
 esac
-
-# Creates the info file
-echo `date` > $backup_path/$backup_type/$backup_name/$info_file
-echo "Excluded:" >> $backup_path/$backup_type/$backup_name/$info_file
-cat .exclude >> $backup_path/$backup_type/$backup_name/$info_file
-
-# Informs that everything is done
-dialog --title "Backup Complete" --msgbox "Backup complete- backup set $backup_name" 10 30
 
 # To Restore
 # Files
