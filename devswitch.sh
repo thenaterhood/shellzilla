d1efba60749cf04d31ba1af13df9bd81d942026a
Nate Levesque <public@thenaterhood.com>
2012-12-19 23:19:16 -0500

    update comment header, add notification function

--- devswitch.sh
+++ devswitch.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #
-#############################
-# Nate Levesque
-# 11/14/2012
-# Switch to turn an input device on and off.  
+# Author: Nate Levesque <public@thenaterhood.com>
+# Language: Shell
+# Filename: devswitch.sh
 #
-#############################
-
+# Description:
+#   turns xinput devices on and off by checking their current status
+#   and changing it to the opposite status.
+#
+########################################################################
+# Settings section                                                     #
+########################################################################
 device_name=touchpad
 # Gets the device id for the device
 deviceid=`xinput | grep -i $device_name | cut -d"=" -f2 | cut -c 1-2`
@@ -25,13 +29,23 @@ if [ ! `command -v xinput` ]; then
     exit 1
 fi
 
+# Defines a common function the script can use for notification
+notify(){
+    export DISPLAY=:0.0 && notify-send \
+    -i /usr/share/icons/gnome/scalable/devices/input-touchpad-symbolic.svg $device_name "$device_name $change"
+    
+    echo "$device_name is now $change"
+}
 # Changes it to the opposite status
 if [ "$status" = 1 ]; then
     xinput set-int-prop $deviceid "Device Enabled" 8 0
-    export DISPLAY=:0.0 && notify-send -i /usr/share/icons/gnome/scalable/devices/input-touchpad-symbolic.svg $device_name "$device_name disabled"
+    change=disabled
+    notify
+    
 elif [ "$status" = 0 ]; then
     xinput set-int-prop $deviceid "Device Enabled" 8 1
-    export DISPLAY=:0.0 && notify-send -i /usr/share/icons/gnome/scalable/devices/input-touchpad-symbolic.svg $device_name "$device_name enabled"
+    change=enabled
+    notify
 fi
 
 exit 0
