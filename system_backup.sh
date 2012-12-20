423fc12f0b469ad1135ba4c5ce1a39c9a2c25f9a
Nate Levesque <public@thenaterhood.com>
2012-12-19 22:24:17 -0500

    improve comments, move things around for readability

--- system_backup.sh
+++ system_backup.sh
@@ -10,13 +10,19 @@
 # TODO:  clean up everything and generally improve the code a lot
 clear
 
+########################################################################
+# Basic settings.  Set these before using the script                   #
+########################################################################
+archive="tgz"
+mountpt="/run/media/$USER"
+
 # Sets appropriate variables
 day=`date +%d`
 month=`date +%b`
 year=`date +%Y`
-archive="tgz"
-mountpt="/run/media/$USER"
 
+
+# Implements a few basic dependency checks
 if [ ! `command -v dialog` ]; then
     echo "Dialog is not installed, and is required for this script. Exiting..."
     exit 1
@@ -32,24 +38,24 @@ if [ ! `command -v tar` ]; then
     exit 1
 fi
 
-# Asks for the drive semi-graphically with dialog and 
+# Asks for the backup drive semi-graphically with dialog
 dialog --backtitle "Backup" --msgbox "Mount media device and hit enter." 5 40
 NUM=$(dialog --output-fd 1 --backtitle "Backup" --radiolist "Select device:" 12 40 4 0 /dev/null off `ls $mountpt | sed 's/ .*//g' | awk '{print NR " " $0 " off"}' | sed ':a;N;$!ba;s/\n/ /g'`)
 drive="`ls $mountpt | head -$NUM | tac | head -1`"
 
-# Parses the data above into a filename for the backup
+# Pieces together filenames for the backup and info
 backup_name="$HOSTNAME-$month-$day-$year"
 backup_file="$HOSTNAME-$month-$day-$year.$archive"
 info_file="$HOSTNAME-$month-$day-$year.info"
 
-# Moves us into the system root
+# Moves into the system root
 cd /
 
-# Asks what backup we want- rsync /home or tar /
+# Ask what backup to perform- rsync /home/$USER or tarball OS
 backup_option=$(dialog --output-fd 1 --backtitle "Backup Options" --radiolist "Select one:" 12 40 4 1 "home sync" off 2 "OS image (requires root)" off)
 backup_path="$mountpt/$drive/backups"
 
-# Runs the actual backup- full partition backup
+# Runs the actual backup
 case "$backup_option" in
     1)
         backup_type="users"
@@ -69,15 +75,13 @@ echo `date` > $backup_path/$backup_type/$backup_name/$info_file
 echo "Excluded:" >> $backup_path/$backup_type/$backup_name/$info_file
 cat .exclude >> $backup_path/$backup_type/$backup_name/$info_file
 
-# Informs us that everything is done
+# Informs that everything is done
 dialog --title "Backup Complete" --msgbox "Backup complete- backup set $backup_name" 10 30
 
 # To Restore
-# - From the rsync
-# Software
-# dpkg --set-selections < /media/Recovery/$DRV/$HOST/installed-software && dselect
 # Files
-# Just make users then replace /home with backup copy
+# Just make users then replace /home with backup copy. Permission 
+# problems may apply
 # - TO RESTORE (from the tarball)
 #  tar xvpfz backup.tgz -C /
 exit 0
