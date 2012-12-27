215f779d02f036178e0d6514149985f3a38b8c95
Nate Levesque <public@thenaterhood.com>
2012-12-27 15:15:45 -0500

    add support for device names as arguments, improve description, change from "if" to "case" blocks

--- devswitch.sh
+++ devswitch.sh
@@ -6,12 +6,42 @@
 #
 # Description:
 #   turns xinput devices on and off by checking their current status
-#   and changing it to the opposite status.
+#   and changing it to the opposite status.  Can be used by manually 
+#   configuring the device ID variable or by using the device's name
+#   by setting the device_name variable, in which case the script
+#   finds the device id itself.  The script supports giving a device
+#   name as an argument, but if none is given will fall back on the
+#   pre-coded default value of "touchpad".
+#
+# Arguments:
+#   name: the name of the device to toggle on or off
+#
+# Examples:
+#   ./devswitch.sh trackpoint - toggle the thinkpad trackpoint on or off
+#   ./devswitch.sh - toggle the default device on or off
 #
 ########################################################################
 # Settings section                                                     #
 ########################################################################
-device_name=touchpad
+default_device=touchpad
+
+# Pick up any arguments given and use that, or fall back on the default
+case "$1" in
+    "")
+    device_name=$default_device
+    ;;
+    
+    *)
+    device_name=$1
+    ;;
+esac
+
+# Checks to make sure script dependencies are satisfied
+if [ ! `command -v xinput` ]; then
+    echo "You need to install xinput to use this script."
+    exit 1
+fi
+
 # Gets the device id for the device
 deviceid=`xinput | grep -i $device_name | cut -d"=" -f2 | cut -c 1-2`
 
@@ -23,12 +53,6 @@ deviceid=`xinput | grep -i $device_name | cut -d"=" -f2 | cut -c 1-2`
 # Gets the current status of the device
 status=`xinput list-props $deviceid | grep "Device Enabled" | cut -c 24`
 
-# Checks to make sure script dependencies are satisfied
-if [ ! `command -v xinput` ]; then
-    echo "You need to install xinput to use this script."
-    exit 1
-fi
-
 # Defines a common function the script can use for notification
 notify(){
     export DISPLAY=:0.0 && notify-send \
@@ -36,16 +60,20 @@ notify(){
     
     echo "$device_name is now $change"
 }
+
 # Changes it to the opposite status
-if [ "$status" = 1 ]; then
+case "$status" in
+    1)
     xinput set-int-prop $deviceid "Device Enabled" 8 0
     change=disabled
     notify
+    ;;
     
-elif [ "$status" = 0 ]; then
+    0)
     xinput set-int-prop $deviceid "Device Enabled" 8 1
     change=enabled
     notify
-fi
+    ;;
+esac
 
 exit 0
