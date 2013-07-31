89e7e7cb3ac0d581913365d28b9b38050a0c76cb
Nate Levesque <public@thenaterhood.com>
2013-07-31 19:32:12 -0400

    update for not spamming the user if running as a cron task

--- battNotification.sh
+++ battNotification.sh
@@ -26,8 +26,28 @@ notify(){
 }
 
 depCheck acpi
-activity="Battery Status"
-notify
+show="n"
+
+# If we're automatically running the script
+# let's not spam the user, only tell them if
+# there is a reason or if no arguments
+case $1 in
+	"auto")
+	if [ `acpi | cut -d"," -f2 | cut -c 2-3` -lt 25 ]; then show="y"; fi
+	;;
+	"")
+	show=""
+	;;
+esac
+
+
+if [ $show="y" ]; then	
+	activity="Battery Status"
+	if [ `acpi | cut -d"," -f2 | cut -c 2-3` -lt 25 ]; then activity="Battery under 25%"; fi
+	if [ `acpi | cut -d"," -f2 | cut -c 2-3` -lt 20 ]; then activity="BATTERY LOW"; fi
+
+	notify
+fi
 
 
 
